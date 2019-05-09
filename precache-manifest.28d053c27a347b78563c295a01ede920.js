ba6b391d2cc481546dc555060fc78a431f27b266
Viktor <meow.lululu@gmail.com>
2019-05-09 07:18:05 +0200

    Updates

---
+++ precache-manifest.28d053c27a347b78563c295a01ede920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "bb6cacf2b0fed5d4c58c",
+    "url": "/contactmanager/static/js/main.b51d35d8.chunk.js"
+  },
+  {
+    "revision": "05d0fda002b68210d2ef",
+    "url": "/contactmanager/static/js/2.4bcfc755.chunk.js"
+  },
+  {
+    "revision": "05d0fda002b68210d2ef",
+    "url": "/contactmanager/static/css/2.77794042.chunk.css"
+  },
+  {
+    "revision": "e3be363a2ff60cdde5897fb9954d930e",
+    "url": "/contactmanager/index.html"
+  }
+];
